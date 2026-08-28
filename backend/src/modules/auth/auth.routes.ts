5bb2162c8fde474b59fb1b74c243964c98c27c79
Nasser Ruiz Rehman <nasser.ruizrehman@gmail.com>
2026-08-28 08:34:52 -0300

    chore: ignore local backend (deprecated) directory

--- backend/src/modules/auth/auth.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { loginController } from "./auth.controller";
-import { authMiddleware } from "../../middlewares/auth.middleware";
-
-
-export const authRoutes = Router();
-
-authRoutes.post('/login', loginController);
--- backend/src/utils/roles.ts
+++
@@ -1,7 +0,0 @@
-export const Roles = {
-    ADMIN: 'ADMIN',
-    AGENT: 'AGENT',
-    USER: 'USER',
-} as const;
-
-export type Role = typeof Roles[keyof typeof Roles];
