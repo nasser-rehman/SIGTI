ca563b440909db2b9c041bffa0a0c4156b4efb89
Nasser Ruiz Rehman <nasser.ruizrehman@gmail.com>
2026-09-04 16:50:28 -0300

    fix(api): register LowestUtilizationStrategy for ITicketAssignmentStrategy in DI

--- SIGTI/src/SIGTI.Application/DependencyInjection.cs
+++ SIGTI/src/SIGTI.Application/DependencyInjection.cs
@@ -5,26 +5,41 @@ using SIGTI.Application.Common.Behaviors;
 using SIGTI.Application.Common.Interfaces.Services;
 using SIGTI.Application.Common.Services;
 using SIGTI.Application.Services;
+using SIGTI.Application.Services.TicketAssignment;
+using SIGTI.Domain.Interfaces.Services;
 
 namespace SIGTI.Application;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApplication(this IServiceCollection services)
+    public static IServiceCollection AddApplication(
+        this IServiceCollection services
+    )
     {
         // MediatR
         services.AddMediatR(cfg =>
         {
-            cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+            cfg.RegisterServicesFromAssembly(
+                typeof(DependencyInjection).Assembly
+            );
             cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
         });
 
         // FluentValidation
-        services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
+        services.AddValidatorsFromAssembly(
+            typeof(DependencyInjection).Assembly
+        );
 
         // Services
         services.AddScoped<IEntityReferenceService, EntityReferenceService>();
-        services.AddScoped<ITechnicianAssignmentService, TechnicianAssignmentService>();
+        services.AddScoped<
+            ITechnicianAssignmentService,
+            TechnicianAssignmentService
+        >();
+        services.AddScoped<
+            ITicketAssignmentStrategy,
+            LowestUtilizationStrategy
+        >();
 
         return services;
     }
